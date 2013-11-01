cae75d9b1efa4d00a792d3b2d07975f23ea2d418
wangshilianghit <wangshilianghit@gmail.com>
2013-11-01 03:21:48 -0400

    first commit

---
+++ src/weibo_reptile.py
@@ -0,0 +1,305 @@
+#!/usr/bin/python
+#-*-coding:utf8-*-
+
+from pprint import pprint
+from weibopy.auth import OAuthHandler
+from weibopy.api import API
+from weibopy.binder import bind_api
+from weibopy.error import WeibopError
+import time,os,pickle,sys
+import logging.config 
+from multiprocessing import Process
+import os
+import simplejson as json
+import time
+import gzip
+
+
+class Weibo_reptile():
+
+    def __init__(self,consumer_key, consumer_secret, json_file):
+
+        self.consumer_key, self.consumer_secret = consumer_key, consumer_secret
+        self.json_file = json_file
+
+    def getAtt(self, key):
+
+        try:
+            return self.obj.__getattribute__(key)
+
+        except Exception, e:
+            print e
+            return ''
+
+    def getAttValue(self, obj, key):
+
+        try:
+            return obj.__getattribute__(key)
+        except Exception, e:
+            print e
+            return ''
+
+    def auth(self):
+
+        if len(self.consumer_key) == 0:
+            print "Please set consumer_key"
+            return
+        
+        if len(self.consumer_key) == 0:
+            print "Please set consumer_secret"
+            return
+        
+        self.auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth_url = self.auth.get_authorization_url()
+        print 'Please authorize: ' + auth_url
+        verifier = raw_input('PIN: ').strip()
+        self.auth.get_access_token(verifier)
+        self.api = API(self.auth)
+
+    def setToken(self, token, tokenSecret):
+
+        self.auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.setToken(token, tokenSecret)
+        self.api = API(self.auth)
+
+    def get_userprofile(self, id):
+
+        try:
+            userprofile = {}
+            userprofile['id'] = id
+            user = self.api.get_user(id)
+            self.obj = user
+            
+            userprofile['screen_name'] = self.getAtt("screen_name")
+            userprofile['name'] = self.getAtt("name")
+            userprofile['province'] = self.getAtt("province")
+            userprofile['city'] = self.getAtt("city")
+            userprofile['location'] = self.getAtt("location")
+            userprofile['description'] = self.getAtt("description")
+            userprofile['url'] = self.getAtt("url")
+            userprofile['profile_image_url'] = self.getAtt("profile_image_url")
+            userprofile['domain'] = self.getAtt("domain")
+            userprofile['gender'] = self.getAtt("gender")
+            userprofile['followers_count'] = self.getAtt("followers_count")
+            userprofile['friends_count'] = self.getAtt("friends_count")
+            userprofile['statuses_count'] = self.getAtt("statuses_count")
+            userprofile['favourites_count'] = self.getAtt("favourites_count")
+            userprofile['created_at'] = self.getAtt("created_at")
+
+        except WeibopError, e:      
+
+            print "error occured when access userprofile use user_id:",id
+            print "Error:",e
+            log.error("Error occured when access userprofile use user_id:{0}\nError:{1}".format(id, e),exc_info=sys.exc_info())
+            return None
+            
+        return userprofile
+
+
+    def get_latest_weibo(self, user_id, count):
+
+        status = []
+        try:            #error occur in the SDK
+            timeline = self.api.user_timeline(count = count, user_id = user_id)
+
+        except Exception as e:
+            print "error occured when access status use user_id:",user_id
+            print "Error:",e
+            log.error("Error occured when access status use user_id:{0}\nError:{1}".format(user_id, e),exc_info=sys.exc_info())
+            return None
+
+        for line in timeline:
+            self.obj = line
+            statusprofile = {}
+            statusprofile['usr_id'] = user_id
+            statusprofile['user'] = self.getAtt("user")
+            statusprofile['id'] = self.getAtt("id")
+            statusprofile['created_at'] = self.getAtt("created_at")
+            statusprofile['text'] = self.getAtt("text")
+            statusprofile['source'] = self.getAtt("source")
+            statusprofile['favorited'] = self.getAtt("favorited")
+            statusprofile['in_reply_to_status_id'] = self.getAtt("in_reply_to_status_id")
+            statusprofile['in_reply_to_user_id'] = self.getAtt("in_reply_to_user_id")
+            statusprofile['in_reply_to_screen_name'] = self.getAtt("in_reply_to_screen_name")
+            statusprofile['geo'] = repr(pickle.dumps(self.getAtt("geo"),pickle.HIGHEST_PROTOCOL))
+            statusprofile['mid'] = self.getAtt("mid")
+            status.append(statusprofile)
+
+        return status
+
+
+    def friends_ids(self,id):
+
+        next_cursor,cursor = 1,0
+        ids = []
+        while(0!=next_cursor):
+            fids = self.api.friends_ids(user_id=id,cursor=cursor)
+            self.obj = fids
+            ids.extend(self.getAtt("ids"))
+            cursor = next_cursor = self.getAtt("next_cursor")
+            previous_cursor = self.getAtt("previous_cursor")
+        return ids
+
+    def manage_access(self):
+
+        info = self.api.rate_limit_status()
+        self.obj = info
+        sleep_time = round( (float)(self.getAtt("reset_time_in_seconds"))/self.getAtt("remaining_hits"),2 ) if self.getAtt("remaining_hits") else self.getAtt("reset_time_in_seconds")
+        print self.getAtt("remaining_hits"),self.getAtt("reset_time_in_seconds"),self.getAtt("hourly_limit"),self.getAtt("reset_time")
+        print "sleep time:",sleep_time,'pid:',os.getpid()
+        time.sleep(sleep_time + 1.5)
+
+    def save_to_file(self, number, userprofile, statuses):
+
+        self.profile_file.write('Profile number: ' + str(number) + '\n')
+
+        for key, value in userprofile.iteritems():
+
+            if key == None or value == None:
+                continue
+
+            if isinstance(value, basestring): 
+                print key  + ': ' + value
+                self.profile_file.write(key + ': ' + value.encode('utf-8'))
+            else:
+                print key + ': ' + str(value)
+                self.profile_file.write(key + ': ' + str(value))
+
+            self.profile_file.write('\n')
+        self.profile_file.write('\n')
+
+        print 'Profile id: ' + userprofile['name'] 
+        self.status_file.write('Profile name: ' + userprofile['name'].encode('utf-8') + '\n')
+
+        for status in statuses: 
+            for key, value in status.iteritems():
+
+                if key == None or value == None:
+                    continue
+
+                if isinstance(value, basestring):
+                    print key  + ': ' + value
+                    self.status_file.write(key + ': ' + value.encode('utf-8'))
+                else:
+                    print key + ': ' + str(value)
+                    self.status_file.write(key + ': ' + str(value))
+
+                self.status_file.write('\n')
+            self.status_file.write('\n')
+        self.status_file.write('\n')
+
+    def save_to_json(self, userprofile, statuses):
+
+        if self.json_file.rfind('/') == -1:
+            file_name = time.strftime("%Y_%m_%d") + '_' + self.json_file
+
+        else:
+            file_name = self.json_file[0: self.json_file.rfind('/') + 1] + time.strftime("%Y_%m_%d") + '_' + self.json_file.split('/')[-1]
+
+        json_file = file(file_name, "ab+") 
+        for status in statuses:
+            json_object = json.dumps(status, ensure_ascii = False).encode('utf-8')
+            json_file.write(json_object)
+            json_file.write("\n")
+        json_file.close()
+
+    def save_to_gzip(self, userprofile, statuses):
+
+        if self.json_file.rfind('/') == -1:
+            file_name = time.strftime("%Y_%m_%d") + '_' + self.json_file
+
+        else:
+            file_name = self.json_file[0: self.json_file.rfind('/') + 1] + time.strftime("%Y_%m_%d") + '_' + self.json_file.split('/')[-1]
+
+        gzip_out = gzip.open(file_name, "ab+") 
+
+        for status in statuses:
+            json_object = json.dumps(status, ensure_ascii = False).encode('utf-8')
+            gzip_out.write(json_object)
+        gzip_out.close()
+
+    def save_data(self, userprofile, status):
+
+        self.collection_statuses.insert(status)
+        self.collection_userprofile.insert(userprofile)
+
+def reptile(weibo_reptile,userid):
+
+    number = 1                #This variable store the number of users that has crawled
+    max_number = 10000000     #The maximum number of user information that needs to crawl
+    user_queue = [userid]     #This list functioned as a queue. It stores the next userid that needs to crawl
+
+    #get 100,000 users info and status
+    for id in user_queue:
+        try:
+            weibo_reptile.manage_access()
+            return_ids = weibo_reptile.friends_ids(id)
+
+            userprofile = weibo_reptile.get_userprofile(id)
+            status = weibo_reptile.get_latest_weibo(count = 200, user_id = id)
+
+            if status is None or userprofile is None:
+                continue
+
+            print 'Already got the data weibo. Save the data now.'
+
+            #Save the data to gzip
+            weibo_reptile.save_to_gzip(userprofile, status)
+
+
+        except Exception as e:
+            log.error("Error occured in reptile,id:{0}\nError:{1}".format(id, e),exc_info=sys.exc_info())
+            time.sleep(60)
+            continue
+
+        number += 1
+        print 'Number of users info and status has cralwed:' + str(number) 
+
+        if number >= max_number:
+            break;
+
+        user_queue.remove(id)
+        user_queue.extend(return_ids)
+
+def run_crawler(consumer_key, consumer_secret, key, secret, userid, json_file):
+
+    try:
+        weibo_reptile = Weibo_reptile(consumer_key, consumer_secret, json_file)
+        weibo_reptile.setToken(key, secret)
+        reptile(weibo_reptile,userid)
+        weibo_reptile.connection.close()
+
+    except Exception as e:
+        print e
+        log.error("Error occured in run_crawler,pid:{1}\nError:{2}".format(os.getpid(), e),exc_info=sys.exc_info())
+
+
+def main():
+    argv = sys.argv[1:]
+
+    if len(argv) != 2:
+        print """
+Usage: %s [filename] [number of threads] 
+""" % (sys.argv[0])
+        sys.exit(1)
+
+    json_file = argv.pop(0)
+    num_thread = argv.pop(0)
+
+    num = 0
+    # Pick correct URL root to use
+    logging.config.fileConfig("logging.conf")
+    with open('apk.txt') as f:
+        for i in f.readlines():
+            if num >= int(num_thread):
+                break 
+            j = i.strip().split(' ')
+            p = Process(target=run_crawler, args=(j[0], j[1], j[2], j[3], j[4], json_file))
+            p.start()
+            num += 1
+
+if __name__ == "__main__":
+
+    log = logging.getLogger('logger_weibo_reptile')
+    main()
+
